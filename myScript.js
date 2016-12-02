c022ba7085466e70f83f7b979a4a46883ab0b6ca
Ricky Avina <rickyavina@icloud.com>
2016-12-02 15:01:13 -0800

    Add files via upload

--- myScript.js
+++ myScript.js
@@ -2,27 +2,22 @@ jq = jQuery.noConflict();
 
 jq(function($){
 
- $('#content').load('home.html');
+ $('#wrapper').load('home.html');
 
 $('a').on('click mouseenter', function(){
-    $(this).css("color", "red");
+    $(this).addClass("active");
 });
 
 $('a').on('mouseleave', function(){
-    $(this).css("color", "white");
+    $(this).removeClass("active");
 });
 
 $('ul li a').on('click', function(){
   var $this =  $(this),
   target = $this.data('target'); console.log("target: " + target);
 
-  $('#content').load(target + '.html');
+  $('#wrapper').load(target + '.html');
   return false;
-
-  if (target == "about"){
-   $("wrapper").css("height", "1400px");
-   $("content").css("height", "1200px")
-  }
 });
 
 
